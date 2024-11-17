e5bcdbb014b6237e71cb100d1cf72f0a5d95c70e
Zied Mabrouk <84295527+Zied-Mabrouk@users.noreply.github.com>
2024-11-17 17:47:37 +0100

    Remove hover animation from mobile

--- src/components/cores/RotatingCard.tsx
+++ src/components/cores/RotatingCard.tsx
@@ -1,5 +1,6 @@
-import React, { ReactNode, useCallback, useState } from 'react';
+import React, { ReactNode, useMemo, useState } from 'react';
 import { Tilt } from 'react-tilt';
+import { isMobile } from 'react-device-detect';
 
 type Props = {
   children: ReactNode;
@@ -8,20 +9,26 @@ type Props = {
 const RotatingCard = ({ children }: Props) => {
   const [flipped, setFlipped] = useState(true);
 
-  const defaultOptions = {
-    reverse: false, // reverse the tilt direction
-    max: 50, // max tilt rotation (degrees)
-    perspective: 1000, // Transform perspective, the lower the more extreme the tilt gets.
-    scale: 1, // 2 = 200%, 1.5 = 150%, etc..
-    speed: 500, // Speed of the enter/exit transition
-    transition: true, // Set a transition on enter/exit.
-    axis: null, // What axis should be disabled. Can be X or Y.
-    reset: true, // If the tilt effect has to be reset on exit.
-    easing: 'cubic-bezier(.03,.98,.52,.99)', // Easing on enter/exit.
-  };
+  const [Element, defaultOptions] = useMemo(
+    () => [
+      isMobile ? React.Fragment : Tilt,
+      {
+        reverse: false, // reverse the tilt direction
+        max: 50, // max tilt rotation (degrees)
+        perspective: 1000, // Transform perspective, the lower the more extreme the tilt gets.
+        scale: 1, // 2 = 200%, 1.5 = 150%, etc..
+        speed: 500, // Speed of the enter/exit transition
+        transition: true, // Set a transition on enter/exit.
+        axis: null, // What axis should be disabled. Can be X or Y.
+        reset: true, // If the tilt effect has to be reset on exit.
+        easing: 'cubic-bezier(.03,.98,.52,.99)', // Easing on enter/exit.
+      },
+    ],
+    []
+  );
 
   return (
-    <Tilt
+    <Element
       options={defaultOptions}
       style={{ width: '100%', display: 'flex', justifyContent: 'center' }}
     >
@@ -40,7 +47,7 @@ const RotatingCard = ({ children }: Props) => {
           </span>
         </button>
       </article>
-    </Tilt>
+    </Element>
   );
 };
 
